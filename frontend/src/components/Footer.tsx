1459ac4f19d85961a5822f545f1ed5d02dc05286
Roman Dvorak <romandvorak@mlab.cz>
2026-01-20 02:14:22 +0100

    Add dummy footer for testing deployment

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,11 +1,7 @@
 export const Footer = () => {
-	const commit = 'test123'
-	const branch = 'master'
-	const short = commit.substring(0, 7)
-	
 	return (
 		<footer className="footer">
-			<div>{branch}@{short}</div>
+			<div>Version: TEST123</div>
 		</footer>
 	)
 }
